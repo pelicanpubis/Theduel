760b3086cf6a31c6c27d47e1ba40f5eb22d2ed12
pelicanpubis <146066847+pelicanpubis@users.noreply.github.com>
2023-09-29 08:39:43 +0200

    Initial commit

---
+++ TheduelAlbin/Player.cs
@@ -0,0 +1,27 @@
+﻿namespace TheduelAlbin
+{
+    internal class Player
+    {
+        public string Name { get; }
+
+
+        //sätter health till 100.
+        public int Health { get; set; } = 100;
+
+
+
+        public Player(string name)
+        {
+
+            Name = name;
+
+        }
+
+        public int RollDamage()
+        {
+            //använder en klass som ska generera ett random nummer och returnerar en random siffra mellan 1 och 50.
+            return new Random().Next(1, 51);
+
+        }
+    }
+}
---
+++ TheduelAlbin/Program.cs
@@ -0,0 +1,24 @@
+﻿//The Duel
+//Printa en snygg header
+// Berätta vilken spelare som börjar
+//Spelarem som börjar attackera den andra spelaren
+//Dra av hälsa från spelaren som blir attacker
+//Den andra spelaren attackerar den första spelaren
+//...
+//När någon har 0 hälsa, printa vem som vann!!
+
+bool isRunning = true;
+while (isRunning) //sätter igång motorn till spelet
+{
+    PrintHeader();
+    break;
+}
+
+
+void PrintHeader()
+{
+
+    Console.WriteLine("*************");
+    Console.WriteLine("THE DULEEEEEL");
+    Console.WriteLine();
+}
